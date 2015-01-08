c84c711391dfaa150b1cbe4f7f60d74de6093eca
victoraldabalde <victoraldabalde@gmail.com>
2015-01-08 11:13:20 -0300

    andando

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -10,8 +10,34 @@
 angular.module('musicaApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
   });
+
+
+
+$(window).load(function(){
+
+  $('#banners-thumbs').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 140,
+    itemMargin: 5,
+    asNavFor: '#banners-carousel'
+  });
+  $('#banners-carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 1000,
+    sync: "#banners-thumbs",
+    start: function(slider){
+      $('body').removeClass('loading');
+    }
+  });
+
+});
