1e0421c523d5574c2ded37ad1ed6da963bbbc904
victoraldabalde <victoraldabalde@gmail.com>
2015-01-13 18:27:24 -0300

    novo cd

--- frontend/app/scripts/controllers/cd.js
+++ frontend/app/scripts/controllers/cd.js
@@ -11,3 +11,42 @@ angular.module('musicaApp')
   .controller('CdCtrl', function ($scope) {
     
   });
+
+
+/*
+$(window).load(function(){
+
+
+  // store the slider in a local variable
+  var $window = $(window),
+      flexslider;
+ 
+  // tiny helper function to add breakpoints
+  function getGridSize() {
+    return (window.innerWidth < 600) ? 2 :
+           (window.innerWidth < 900) ? 3 : 4;
+  }
+ 
+  
+   $('#playlist-musics-list').flexslider({
+    animation: 'slide',
+    animationLoop: false,
+    slideshow: false,
+    controlNav: false,
+    itemWidth: 203,
+    itemMargin: 10,
+    minItems: getGridSize(), // use function to pull in initial value
+    maxItems: getGridSize() // use function to pull in initial value
+  });
+  
+  
+  // check grid size on resize event
+  $window.resize(function() {
+    var gridSize = getGridSize();
+ 
+    flexslider.vars.minItems = gridSize;
+    flexslider.vars.maxItems = gridSize;
+  });
+  
+
+});*/
