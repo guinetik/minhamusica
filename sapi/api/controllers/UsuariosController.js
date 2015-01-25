f744bbad1a9aa86a7f90eaf6f375c94bfaced2ca
Guinetik <email@guinetik.com>
2015-01-25 20:09:39 -0300

    corrigindo um bug ao trazer os cds do usuário em perfil

--- sapi/api/controllers/UsuariosController.js
+++ sapi/api/controllers/UsuariosController.js
@@ -15,9 +15,9 @@ module.exports = {
   },
   lookup: function (req, res) {
     var token = req.body.token;
-    if(token == null) res.status(403).send({message: 'please provide a token'});
-    Usuarios.findOneByToken(token, function(result){
-      if(result) {
+    if (token == null) res.status(403).send({message: 'please provide a token'});
+    Usuarios.findOneByToken(token, function (result) {
+      if (result) {
         return res.status(200).send({
           message: 'User found',
           data: result.toJSON()
@@ -29,36 +29,25 @@ module.exports = {
       }
     });
   },
-  perfil: function(req,res){
-
-	 var id = req.query.id;
-	  var collection = {};
-
-	  Usuarios.find({id:id}).exec(function findCB(err,usuario){
-	  	if(err) return res.status(500).send({message : 'Erro ao buscar usuário.'});
-
-		if(usuario.length){
-			collection.usuario = usuario;
-
-			Cd.find({artista:usuario.id}).sort('createdAt DESC').limit(10).exec(function(err2, c) {
-				if(err2) return res.status(500).send({message : 'ultimos cds do usuario.'});
-
-				collection.cds = c;
-
-				Eventos.find({usuario:usuario.id}).sort('createdAt DESC').limit(10).exec(function(err3, e) {
-					if(err3) return res.status(500).send({message : 'ultimos eventos do usuario.'});
-
-					collection.eventos = e;
-					return res.status(200).send({perfil:collection});
-				});
-			});
-		}else
-			return res.status(404).send({message:'Usuário não encontrado!'});
-
-	  });
-
+  perfil: function (req, res) {
+    var id = req.query.id;
+    var collection = {};
+    Usuarios.findOne({id: id}).exec(function findCB(err, usuario) {
+      if (err) return res.status(500).send({message: 'Erro ao buscar usuário.'});
+      if (usuario.id) {
+        collection.usuario = usuario;
+        console.log("user.id", usuario.id);
+        Cd.find().where({'artista': usuario.id}).sort('createdAt DESC').limit(10).exec(function (err2, c) {
+          if (err2) return res.status(500).send({message: 'ultimos cds do usuario.'});
+          collection.cds = c;
+          Eventos.find().where({usuario: usuario.id}).sort('createdAt DESC').limit(10).exec(function (err3, e) {
+            if (err3) return res.status(500).send({message: 'ultimos eventos do usuario.'});
+            collection.eventos = e;
+            return res.status(200).send({perfil: collection});
+          });
+        });
+      } else return res.status(404).send({message: 'Usuário não encontrado!'});
+    });
   }
-
-
 };
 
