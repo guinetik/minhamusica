0558f1781c531deec9decfd4c5c839effa53a127
Guinetik <email@guinetik.com>
2015-01-27 02:33:13 -0300

    implementando página do cd

--- frontend/app/scripts/controllers/cd.js
+++ frontend/app/scripts/controllers/cd.js
@@ -7,7 +7,17 @@
  * # MainCtrl
  * Controller of the musicaApp
  */
-angular.module('musicaApp').controller('CdCtrl', ['$scope', 'api', CdCtrl]);
-function CdCtrl($scope, api) {
-
+angular.module('musicaApp').controller('CdCtrl', ['$scope', 'api', '$timeout', '$stateParams', CdCtrl]);
+function CdCtrl($scope, api, $timeout, $stateParams) {
+  $scope.cd = {};
+  $scope.cd.id = $stateParams.id;
+  $scope.$on('$viewContentLoaded', function (event) {
+    $timeout(function () {
+      api.getCD($scope.cd.id, function (result) {
+        if (result.status == 200) {
+          $scope.cd = result;
+        }
+      });
+    });
+  });
 }
