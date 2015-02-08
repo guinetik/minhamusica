0dcbba06bf7538d99deae06933352f7fcd5a8f85
Guinetik <email@guinetik.com>
2015-02-08 11:08:46 -0300

    adicionando eventos (front)

--- api/models/Eventos.js
+++ api/models/Eventos.js
@@ -6,33 +6,50 @@
  */
 
 var Eventos = module.exports = {
-  attributes: {
-    titulo: {
-      type: 'string',
-      required: true
-    },
-    descricao: {
-      type: 'string',
-      required: true
-    },
-    inicio: {
-      type: 'date',
-      required: true
-    },
-    fim: {
-      type: 'date',
-      required: true
-    },
-    usuario: {
-      model: 'usuarios',
-      required: true
-    },
-    toJSON: function () {
-      var obj = this.toObject();
-      delete obj.createdAt;
-      delete obj.updatedAt;
-      return obj;
+    schema: true,
+    attributes: {
+        nome: {
+            type: 'string',
+            required: true
+        },
+        descricao: {
+            type: 'string',
+            required: true
+        },
+        local: {
+            type: 'string',
+            required: true
+        },
+        cidade: {
+            model: 'Cidades',
+            required:true
+        },
+        inicio: {
+            type: 'date',
+            required: true
+        },
+        fim: {
+            type: 'date',
+            required: true
+        },
+        usuario: {
+            model: 'Usuarios',
+            required: true
+        },
+        imagem: {
+            type: 'string',
+            required: true
+        },
+        link: {
+            type: 'string',
+            required: true
+        },
+        toJSON: function () {
+            var obj = this.toObject();
+            delete obj.createdAt;
+            delete obj.updatedAt;
+            return obj;
+        }
     }
-  }
 };
 
